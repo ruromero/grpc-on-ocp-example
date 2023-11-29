219d758c2ca6562c539c4fb3d145aaa3a8777684
Ruben Romero Montes <rromerom@redhat.com>
2023-11-29 23:04:19 +0100

    initial commit
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

---
+++ src/main/java/com/github/ruromero/HelloService.java
@@ -0,0 +1,16 @@
+package com.github.ruromero;
+
+import com.github.ruromero.grpc.Greeter;
+import com.github.ruromero.grpc.HelloReply;
+import com.github.ruromero.grpc.HelloRequest;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+
+@GrpcService
+public class HelloService implements Greeter {
+
+    @Override
+    public Uni<HelloReply> sayHello(HelloRequest request) {
+        return Uni.createFrom().item(() -> HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
+    }
+}
